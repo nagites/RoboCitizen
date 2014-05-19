273be97087d733a9f167c67dee79e864d4a8c211
Cfeusier <cfeusier@gmail.com>
2014-05-19 08:28:27 -0700

    Refactor index.html.erb into partials

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,7 +10,148 @@ class HomeController < ApplicationController
   
   def index
   end
-  
+
+  def alabama
+    render 'home/states/_alabama'
+  end
+  def alaska
+    render 'home/states/_alaska'
+  end
+  def arkansas
+    render 'home/states/_arkansas'
+  end
+  def arizona
+    render 'home/states/_arizona'
+  end
+  def california
+    render 'home/states/_california'
+  end
+  def colorado
+    render 'home/states/_colorado'
+  end
+  def connecticut
+    render 'home/states/_connecticut'
+  end
+  def delaware
+    render 'home/states/_delaware'
+  end
+  def florida
+    render 'home/states/_florida'
+  end
+  def georgia
+    render 'home/states/_georgia'
+  end
+  def hawaii
+    render 'home/states/_hawaii'
+  end
+  def indiana
+    render 'home/states/_indiana'
+  end
+  def iowa
+    render 'home/states/_iowa'
+  end
+  def kansas
+    render 'home/states/_kansas'
+  end
+  def kentucky
+    render 'home/states/_kentucky'
+  end
+  def louisiana
+    render 'home/states/_louisiana'
+  end
+  def maine
+    render 'home/states/_maine'
+  end
+  def maryland
+    render 'home/states/_maryland'
+  end
+  def massachussetts
+    render 'home/states/_massachussetts'
+  end
+  def michigan
+    render 'home/states/_michigan'
+  end
+  def minnesota
+    render 'home/states/_minnesota'
+  end
+  def mississippi
+    render 'home/states/_mississippi'
+  end
+  def missouri
+    render 'home/states/_missouri'
+  end
+  def montana
+    render 'home/states/_montana'
+  end
+  def nebraska
+    render 'home/states/_nebraska'
+  end
+  def nevada
+    render 'home/states/_nevada'
+  end
+  def newhampshire
+    render 'home/states/_newhampshire'
+  end
+  def newjersey
+    render 'home/states/_newjersey'
+  end
+  def newmexico
+    render 'home/states/_newmexico'
+  end
+  def newyork
+    render 'home/states/_newyork'
+  end
+  def northcarolina
+    render 'home/states/_northcarolina'
+  end
+  def northdakota
+    render 'home/states/_northdakota'
+  end
+  def ohio
+    render 'home/states/_ohio'
+  end
+  def oklahoma
+    render 'home/states/_oklahoma'
+  end
+  def oregon
+    render 'home/states/_oregon'
+  end
+  def pennsylvania
+    render 'home/states/_pennsylvania'
+  end
+  def rhodeisland
+    render 'home/states/_rhodeisland'
+  end
+  def southcarolina
+    render 'home/states/_southcarolina'
+  end
+  def tennessee
+    render 'home/states/_tennessee'
+  end
+  def texas
+    render 'home/states/_texas'
+  end
+  def utah
+    render 'home/states/_utah'
+  end
+  def vermont
+    render 'home/states/_vermont'
+  end
+  def virginia
+    render 'home/states/_virginia'
+  end
+  def washington
+    render 'home/states/_washington'
+  end
+  def westvirginia
+    render 'home/states/_westvirginia'
+  end
+  def wisconsin
+    render 'home/states/_wisconsin'
+  end
+  def wyoming
+    render 'home/states/_wyoming'
+  end
   ################################################################
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,56 +1,54 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  root 'home#index'
+  get 'home/alabama' => 'home#alabama'
+  get 'home/alaska' => 'home#alaska'
+  get 'home/arkansas' => 'home#arkansas'
+  get 'home/arizona' => 'home#arizona'
+  get 'home/california' => 'home#california'
+  get 'home/colorado' => 'home#colorado'
+  get 'home/connecticut' => 'home#connecticut'
+  get 'home/delaware' => 'home#delaware'
+  get 'home/florida' => 'home#florida'
+  get 'home/georgia' => 'home#georgia'
+  get 'home/hawaii' => 'home#hawaii'
+  get 'home/indiana' => 'home#indiana'
+  get 'home/iowa' => 'home#iowa'
+  get 'home/kansas' => 'home#kansas'
+  get 'home/kentucky' => 'home#kentucky'
+  get 'home/louisiana' => 'home#louisiana'
+  get 'home/maine' => 'home#maine'
+  get 'home/maryland' => 'home#maryland'
+  get 'home/massachussetts' => 'home#massachussetts'
+  get 'home/michigan' => 'home#michigan'
+  get 'home/minnesota' => 'home#minnesota'
+  get 'home/mississippi' => 'home#mississippi'
+  get 'home/missouri' => 'home#missouri'
+  get 'home/montana' => 'home#montana'
+  get 'home/nebraska' => 'home#nebraska'
+  get 'home/nevada' => 'home#nevada'
+  get 'home/newhampshire' => 'home#newhampshire'
+  get 'home/newjersey' => 'home#newjersey'
+  get 'home/newmexico' => 'home#newmexico'
+  get 'home/newyork' => 'home#newyork'
+  get 'home/northcarolina' => 'home#northcarolina'
+  get 'home/northdakota' => 'home#northdakota'
+  get 'home/ohio' => 'home#ohio'
+  get 'home/oklahoma' => 'home#oklahoma'
+  get 'home/oregon' => 'home#oregon'
+  get 'home/pennsylvania' => 'home#pennsylvania'
+  get 'home/rhodeisland' => 'home#rhodeisland'
+  get 'home/southcarolina' => 'home#southcarolina'
+  get 'home/tennessee' => 'home#tennessee'
+  get 'home/texas' => 'home#texas'
+  get 'home/utah' => 'home#utah'
+  get 'home/vermont' => 'home#vermont'
+  get 'home/virginia' => 'home#virginia'
+  get 'home/washington' => 'home#washington'
+  get 'home/westvirginia' => 'home#westvirginia'
+  get 'home/wisconsin' => 'home#wisconsin'
+  get 'home/wyoming' => 'home#wyoming'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  root to: redirect('/legislators')
+  get '/legislators', to: "home#index"
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
